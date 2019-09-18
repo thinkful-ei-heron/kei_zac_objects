48074bb9469e66f35e20187e0a1c68c8eaf5c656
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-09-18 14:16:57 -0400

    started Q8

--- object_drills/object_drills.js
+++ object_drills/object_drills.js
@@ -192,3 +192,20 @@ characters.find(function(element) {
 
 const hobbits = characters.filter(element => element.race === 'Hobbit');
 const attackGreaterThanFive = characters.filter(element => element.attack > 5);
+
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query){
+  for (let i = 0; i < arr.length; i++){
+    for (Object.keys(query)){
+      
+    }
+}

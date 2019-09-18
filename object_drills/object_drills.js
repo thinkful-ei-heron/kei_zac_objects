0c684e985debc56efe7a8db7a28768bd665af63e
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-09-18 11:59:55 -0400

    self reference

--- object_drills/object_drills.js
+++ object_drills/object_drills.js
@@ -18,3 +18,16 @@ function updateObject(obj) {
   obj.bang = 'bang';
   return obj;
 }
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function(){
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+  return person;
+}

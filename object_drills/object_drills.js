b62287cc7a670563797593abc3f92d276575618d
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-09-18 11:54:57 -0400

    object creator

---
+++ object_drills/object_drills.js
@@ -0,0 +1,12 @@
+'use strict';
+function createMyObject(){
+  const obj = {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function(){
+      return 'hello';
+    }
+  };
+  return obj;
+}

48ec931b5879895042a9051eeee747e7d0a738c3
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-09-18 12:24:41 -0400

    loaf initialized

--- object_drills/object_drills.js
+++ object_drills/object_drills.js
@@ -71,3 +71,14 @@ function validateKeys(obj, expectedKeys) {
   return true;
 }
 
+const loaf = {
+  flour: 300,
+  water: 210,
+};
+//console.log(loaf.flour);
+//console.log(loaf.water);
+loaf.hydration = function() {};
+loaf.hydration = function() {
+  return loaf.water/loaf.flour*100;
+};
+//console.log(loaf.hydration());
